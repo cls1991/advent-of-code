37e542fa3f2ed8d49182e03bb70d721df4232ef2
cls1991 <tzk19910406@hotmail.com>
2017-12-18 11:47:02 +0800

    day1 part two

--- day1/solution.py
+++ day1/solution.py
@@ -1,7 +1,8 @@
 # coding: utf8
 
 
-def inverse_captcha(data):
+def inverse_captcha_v1(data):
+    """Part One"""
     s_sum = 0
     ll = len(data)
     for i in xrange(0, ll):
@@ -11,7 +12,19 @@ def inverse_captcha(data):
     return s_sum
 
 
+def inverse_captcha_v2(data):
+    """Part Two"""
+    s_num = 0
+    ll = len(data)
+    for i in xrange(0, ll):
+        if data[i] == data[(i + ll / 2) % ll]:
+            s_num += int(data[i])
+
+    return s_num
+
+
 if __name__ == '__main__':
     with open('input.txt') as f:
         source = f.read()
-    print(inverse_captcha(source))
+    print('Solution of Day 1 Part One: {0}'.format(inverse_captcha_v1(source)))
+    print('Solution of Day 1 Part Two: {0}'.format(inverse_captcha_v2(source)))
