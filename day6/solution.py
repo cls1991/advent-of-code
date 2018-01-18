c4aee0a54952f2d31a9e0900dafa7231967e0909
cls1991 <tzk19910406@hotmail.com>
2018-01-18 21:25:03 +0800

    day 6 part2

--- day6/solution.py
+++ day6/solution.py
@@ -2,7 +2,7 @@
 
 
 def memory_reallocation_v1(s):
-    """Memory Reallocation."""
+    """memory reallocation."""
     cycles = 0
     banks = s[:]
     ll = len(banks)
@@ -28,9 +28,37 @@ def memory_reallocation_v1(s):
     return cycles
 
 
+def memory_reallocation_v2(s):
+    """memory reallocation."""
+    cycles = 0
+    banks = s[:]
+    ll = len(banks)
+    seen = {}
+    uk = ''.join(list(map(str, banks)))
+
+    while uk not in seen:
+        seen[uk] = cycles
+        key_chosen = banks.index(max(banks))
+        value_chosen = banks[key_chosen]
+        banks[key_chosen] = 0
+
+        while value_chosen > 0:
+            value_chosen -= 1
+            key_chosen += 1
+            if key_chosen >= ll:
+                key_chosen = 0
+            banks[key_chosen] += 1
+
+        uk = ''.join(list(map(str, banks)))
+        cycles += 1
+
+    return cycles - seen[uk]
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         test = list(map(int, f.read().strip().split('\t')))
 
     print('Solution of Day 6 Part One: {0}'.format(memory_reallocation_v1(test)))
+    print('Solution of Day 6 Part Two: {0}'.format(memory_reallocation_v2(test)))
 
